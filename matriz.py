3c96268950a968e4e4a0d3fba599214dfdccc61e
Daniela Correa Muñoz <dacomu24@gmail.com>
2025-03-13 09:02:36 -0500

    Refactor matrix operations and add elimination and row interchange methods

--- matriz.py
+++ matriz.py
@@ -1,4 +1,4 @@
-matriz_aumentada = [[5, 6, 1, 4], [8, 5, 4, 5], [7, 1, 7, 6], [5, 4, 4, 7]]
+matriz = [[5, 6, 1, 4], [8, 5, 4, 5], [7, 1, 7, 6], [5, 4, 4, 7]]
 terminos_independientes = [4, 5, 6, 7]
 
 #Imprime la lista en el formato de una matriz, para una mejor visualizacion
@@ -9,46 +9,89 @@ def imprime_matriz(matriz):
         print("")
 
 print("Matriz aumentada sin ordenar")
-imprime_matriz(matriz_aumentada)
+imprime_matriz(matriz)
 
-#Calcula el valor maximo de cada fila y lo almacena en una lista
-def calc_factores_escala(matriz):
+def calc_factores_escala(matriz, iteracion):
     factores_escala = []
-    for i in matriz:
-        #Convertimos todos los valores de la fila en valores absolutos
-        valores_absolutos = [abs(n) for n in i]
-        #Ordenamos la fila de mayor a menor para obtener el factor de escala, es decir, el mayor valor de cada fila
+    for i in range(iteracion, len(matriz)):
+        fila = matriz[i]
+        valores_absolutos = [abs(j) for j in fila]
         factor_escala = sorted(valores_absolutos, reverse=True)[0]
-        #Almacenamos el factor de escala en una lista y la retornamos para su uso posterior
         factores_escala.append(factor_escala)
     print(factores_escala)
     return factores_escala
 
-#Calculo de los cocientes rk para la primera columna
-def calc_cocientes(matriz, factores_escala):
+# Calculo de los cocientes rk para cada columna
+def calc_cocientes(matriz, factores_escala, iteracion):
     print("Matriz aumentada desde el metodo de cocientes")
     imprime_matriz(matriz)
     cocientes = []
     max_cociente = 0
     max_factor_escala = 0
     fila_max_factor_escala = 0
-    #Llamamos a todos los terminos de la primera columna y los dividimos por los factores de escala
-    for i in range(len(matriz)):
-        #Para obtener los cocientes, debemos redondear el valor absoluto de la division de los terminos de la primera columna entre los factores de escala
-        cociente = round(abs(matriz[i][0]) / factores_escala[i],3)
+    index = 0
+    for i in range(iteracion, len(matriz)):
+        cociente = round(abs(matriz[i][iteracion]) / factores_escala[index],3)
         cocientes.append(cociente)
-        #Obtenemos el maximo cociente y el factor de escala correspondiente en cada iteracion
-        if cociente >= max_cociente and factores_escala[i] > max_factor_escala:
+        print("")
+        if cociente >= max_cociente and factores_escala[index] > max_factor_escala:
+            print("Entre al if (cociente, factor escala): ", cociente, factores_escala[index])
             max_cociente = cociente
-            max_factor_escala = factores_escala[i]
-            fila_max_factor_escala = i
+            max_factor_escala = factores_escala[index]
+            fila_max_factor_escala = index
+            index += 1
+
     print("Cocientes", cocientes)
     print("Maximo cociente", max_cociente)
     print("Maximo factor de escala", max_factor_escala)
     print("Fila con maximo factor de escala", fila_max_factor_escala)
     return cocientes, max_cociente, max_factor_escala, fila_max_factor_escala
-    
-print("Factores escala")
-factores_escala = calc_factores_escala(matriz_aumentada)
-print("Tamaño de la matriz", len(matriz_aumentada))
-calc_cocientes(matriz_aumentada, factores_escala)
+
+def intercambio_fila(matriz_actual, iteracion, fila_max_factor_escala):
+
+    print("Matriz en el intercambio de filas: ")
+    imprime_matriz(matriz_actual)
+
+    fila_actual = matriz_actual[iteracion]
+    fila_mejor_pivote = matriz_actual[fila_max_factor_escala]
+    print("Fila actual | Fila mejor pivote: ", fila_actual, fila_mejor_pivote)
+
+    #Intercambio de filas
+    matriz_actual[iteracion] = fila_mejor_pivote
+    matriz_actual[fila_max_factor_escala] = fila_actual
+
+    print("Matriz intercambiada: ")
+    imprime_matriz(matriz_actual)
+    return matriz_actual
+
+def eliminacion(matriz_actual, iteracion, pivote):
+    print("Entramos en el metodo de eliminacion")
+    fila_pivote = matriz_actual[iteracion]
+    print("Pivote en iteracion:", pivote)
+    for i in range(iteracion, len(matriz_actual)):
+        fila = matriz_actual[i + 1]
+        print("Fila: ", fila)
+        for j in range(0, len(fila)):
+            multiplicador = (fila[j]/pivote) * fila_pivote[j]
+            nuevo_valor = matriz_actual[fila][j] - multiplicador
+            matriz_actual[fila][j] = nuevo_valor
+            print("multiplicador: ", multiplicador)
+            print("matriz tras eliminacion en paso: " , j)
+            imprime_matriz(matriz_actual)
+
+            
+        # print("Fila | Multiplicador : ", fila, multiplicador)
+
+
+
+
+
+for iteracion in range (0, len(matriz) - 1):
+    nueva_matriz = []
+    if len(nueva_matriz) > 0: 
+        matriz = nueva_matriz
+
+    factores = calc_factores_escala(matriz, iteracion)
+    cocientes, max_cociente, max_factor_escala, fila_max_factor_escala = calc_cocientes(matriz, factores, iteracion)    
+    nueva_matriz = intercambio_fila(matriz, iteracion, fila_max_factor_escala)
+    nueva_matriz = eliminacion(nueva_matriz, iteracion, max_factor_escala )
